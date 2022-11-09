58a8116c48913413ed52d87870964965ce69f751
David Braun <david.braun@fielmann.com>
2022-11-09 10:24:26 +0100

    [Class-6] use api function for project creation in e2e createIssue test

--- cypress/e2e/createIssue.cy.js
+++ cypress/e2e/createIssue.cy.js
@@ -12,7 +12,7 @@ describe('Create Issue', () => {
 
   beforeEach(() => {
     cy.login()
-    cy.gui_createProject(issue.project)
+    cy.api_createProject(issue.project)
   })
 
   it('successfully', () => {
